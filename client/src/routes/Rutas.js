d0478beec86d8b7a0c1784188bc70e4e1f252a23
Max23strm <maximiliano.ovejak@gmail.com>
2022-09-13 17:05:40 -0500

    add/client/redux actions, reducer y store. Creacion de rutas react-dom-router Modificaciones al nav y creacion de details

--- client/src/routes/Rutas.js
+++ client/src/routes/Rutas.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
-import { Home } from '../views/index'
+import { Home, Details } from '../views/index'
 const Rutas = () => {
     return (
         <BrowserRouter>
