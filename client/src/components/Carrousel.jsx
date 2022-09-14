38446518990c15751494a3c9b5c8534c9e44d038
Max23strm <maximiliano.ovejak@gmail.com>
2022-09-14 17:00:02 -0500

    add nuevo componente promocion Kustom al home y detalles al navbar

---
+++ client/src/components/Carrousel.jsx
@@ -0,0 +1,20 @@
+import { PreviewCard } from './index'
+
+const Carrousel = ({ coleccion }) => {
+    let products = []
+    return (
+        <section className='flex w-10/12 py-3 xl:pl-[16.3rem] flex-row gap-10 justify-start xl:justify-center mx-auto mt-24 overflow-x-auto overflow-y-hidden'>
+            {products?.forEach((data) => {
+                data.collection === coleccion && (<PreviewCard data={data} />)
+            })}
+            <PreviewCard />
+            <PreviewCard />
+            <PreviewCard />
+            <PreviewCard />
+            <PreviewCard />
+            <PreviewCard />
+        </section>
+    )
+}
+
+export default Carrousel
