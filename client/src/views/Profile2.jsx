251a68f55e76786bb4483a68f007d9544332bd21
tomasfv <tomasfernandezvaldes@gmail.com>
2022-09-16 16:31:23 -0300

    tomasProfileCard

--- client/src/views/Profile2.jsx
+++ client/src/views/Profile2.jsx
@@ -14,18 +14,18 @@ export const Profile2 = () => {
   return (
     isAuthenticated && (
       //<div className="border-2 p-6 m-60 font-bold text-xs grid gap-2 justify-items-center text-main-dark dark:text-main-light">
-        <div className="border-2 p-6 m-60 font-bold bg-gris-light grid gap-3 justify-items-start">
-          <div className="flex flex-row">
-            <img src={user.picture} alt={user.name} className='h-20 w-20 rounded-full' />
-            <p className="text-xl ">Perfil</p>
-          </div>
+        <div className="m-60 w-65 flex flex-row justify-center  bg-gris-light dark:bg-gris-dark ">
+        <img src={user.picture} alt={user.name} className='h-20 w-20 rounded-full mt-12' /> 
+
+      <div className="flex flex-col p-8 ml-10 font-bold  bg-gris-light dark:bg-gris-dark text-main-dark dark:text-main-light ">
+        <p className="text-2xl">Perfil</p>
         <p>usuario: {user.name}</p>
         <p>e-mail: {user.email}</p>
         <p>nombre: {user.given_name}</p>
         <p>apellido: {user.family_name}</p>
         <LogoutButton/>
-
       </div>
+        </div>
     )
   );
   
