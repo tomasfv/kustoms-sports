b6a2ef9ca835c7bab95596ae5267a5fd1a56ad56
Kenneth2804 <98670850+Kenneth2804@users.noreply.github.com>
2022-09-17 16:05:12 -0700

    Nodemailer Register

---
+++ api/src/routes/controlers/email-controller.js
@@ -0,0 +1,15 @@
+const nodemailer = require ("nodemailer");
+var transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com", // hostname
+    port: 587, // port for secure SMTP
+    secureConnection: false,
+    tls: {
+       ciphers:'SSLv3'
+    },
+    auth: {
+        user: 'kustomssports@hotmail.com',
+        pass: 'Nightchanges'
+    }
+});
+
+module.exports = {transporter}
