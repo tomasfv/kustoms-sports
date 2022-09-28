00ac6d3a42b8c65486e83058eed20d597b0a89b9
Abrahanm7 <abrahanicolasmol98@gmail.com>
2022-09-28 14:40:00 -0300

    mejorando cosas del front

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -12,7 +12,8 @@ const Dashboard = () => {
  
   const dispatch = useDispatch()
   const [addproduct, setAddProduct] = useState({
-    clotheType: "",
+        id:"",
+        clotheType: "",
         brand: "",
         name: "",
         gender: "Masculino",
@@ -21,9 +22,9 @@ const Dashboard = () => {
         color:"",
         size:"S",
         image:[],
-        stock:"",
-        price:"",
-        promotion:"0,5"
+        stock:0,
+        price:0,
+        promotion:0.16
   });
   function handleClickD(e) {
     setProductos(!productos);
@@ -40,7 +41,7 @@ const Dashboard = () => {
   function postProduct (){
     dispatch(postAddProduct(addproduct))
   }
-  console.log(addproduct)
+  console.log("producto",addproduct)
   
   return (
     <div className="mt-[150px] ml-[250px] mb-[100px] flex flex-row">
@@ -290,7 +291,7 @@ const Dashboard = () => {
                   </div>  
                   <div>
                       <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Precio</label>
-                      <input type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$20000"  required onChange={(e)=>handleChange(e)} name="price" value={addproduct.price}/>
+                      <input type="number" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$20000"  required onChange={(e)=>handleChange(e)} name="price" value={addproduct.price}/>
                   </div>
                   <div>
                       <label for="website" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Colores del Producto</label>
@@ -333,7 +334,7 @@ const Dashboard = () => {
               </div>
               
               
-              <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={postProduct()}>Submit</button>
+              <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={postProduct}>Submit</button>
           </form>
               <div>
                 <Cloudinary addproduct={addproduct}/>
