fc4b2a82955024305a67a06c72616522ec26257b
Abrahanm7 <abrahanicolasmol98@gmail.com>
2022-09-30 16:51:55 -0300

    About

---
+++ client/src/components/About.jsx
@@ -0,0 +1,150 @@
+const About = () => {
+  return (
+
+    <div>
+        
+<section class="mb-10 mt-16 min-h-screen">
+  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+      <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+          <h2 class="my-5 text-3xl text-main-dark dark:text-main-light ">Kustoms Sports</h2>
+          <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400"></p>
+      </div> 
+      <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media-exp1.licdn.com/dms/image/C4D03AQH6zqPT9oYAGw/profile-displayphoto-shrink_200_200/0/1659055690253?e=1669248000&v=beta&t=V2394dbVW__gTdfLa5esStTaAw00tY8AAq4WyHOGtZY" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/agustin-tomas-velazquez-1b2173245/">Agustin Tomas Velazquez</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/agustin-tomas-velazquez-1b2173245/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/Agusv22" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media-exp1.licdn.com/dms/image/C4D03AQGA2QKuAE3d0Q/profile-displayphoto-shrink_200_200/0/1660020868626?e=1669248000&v=beta&t=vGOrOs1VSeqZThM6gvLjOfrHvkxZXAIWkXpVHKB3gtY" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/abraham-molina-8b9711221/">Abraham Nicolas Molina</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/abraham-molina-8b9711221/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/Abrahanm7" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media-exp1.licdn.com/dms/image/C4E03AQFjWnJI8pO4Mw/profile-displayphoto-shrink_200_200/0/1625063772561?e=1669248000&v=beta&t=yrrG-yJUA4TzlYwcBYWAv9euEpmLY9TIbmgoYImOceI" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/tomasferval/">Tomas Fernandez Valdes</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/tomasferval/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/tomasfv" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media-exp1.licdn.com/dms/image/C4D03AQHY64khlXGhCg/profile-displayphoto-shrink_200_200/0/1659118120346?e=1669248000&v=beta&t=eJ4PWapl5hfGCR9-JWNRxVrz_-PLxAITBhHHzEUBboM" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/carlos-cernigoi-a836164/">Carlos Cernigoi</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/carlos-cernigoi-a836164/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/CarlosCernigoi" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media-exp1.licdn.com/dms/image/C4E03AQHAbk2Wetl4bA/profile-displayphoto-shrink_200_200/0/1660861212411?e=1669248000&v=beta&t=_vQmatEc_b3z8o1b101sK6N5CsyNpz94Fkdjqm19srI" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/kenneth-flores-calvillo-28aa38242/">Kenneth Flores Calvillo</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/kenneth-flores-calvillo-28aa38242/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/Kenneth2804" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://media-exp1.licdn.com/dms/image/D4E35AQHeATTp8VTxOg/profile-framedphoto-shrink_200_200/0/1659659681793?e=1664658000&v=beta&t=Wqio_Sq3NoyZF5IORx1B6K9ExTvFvDdQsDLO4o3ZeFk" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/m-ovejak/">Maximiliano Ovejak</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/m-ovejak/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/Max23strm" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/34d6e59d3c72a61be090374bff71a9bd~c5_100x100.jpeg?x-expires=1664222400&x-signature=2cLTaT9lAxsMMGTOMzMKlFghIrs%3D" alt="Bonnie Avatar"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/agustin-tomas-velazquez-1b2173245/">Luca Haller</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://github.com/lucahaller" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          
+          
+      </div>  
+  </div>
+</section>
+    </div>
+    
+    )
+}
+export default About;
