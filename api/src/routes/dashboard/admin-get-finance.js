d31e1352a417290c80f339249d43a239bf6127ec
Kenneth2804 <98670850+Kenneth2804@users.noreply.github.com>
2022-10-01 14:09:46 -0700

    Finances witrh email and pictures

--- api/src/routes/dashboard/admin-get-finance.js
+++ api/src/routes/dashboard/admin-get-finance.js
@@ -1,31 +1,42 @@
 const { Router } = require("express");
-const {Carts, Products} = require("../../db.js");
+const {Carts, Products, Users} = require("../../db.js");
 require("dotenv").config();
-
 const router = Router();
 
 router.get("/", async (req, res) => {
   try {
     let financial = await Carts.findAll({
       where :{ open: "false"},
-      include :{
-        model: Products,
-        /* model: Users */
-      },
+      include :[{
+        model: Products ,
+        model: Users 
+      }],
     });
 
-  const finduserid = financial.map(e => { return{
+  const finduserid = financial.map((e)  => {
 
+    return{
   userId: e.userId,
   totalamount: e.totalamount,
   updatedAt: e.updatedAt,
-  products : e.products.map(t => { return { promotion: t.promotion }}),
-  name : e.products.map(t => { return { name: t.name }}),
-  /* email: e.users.email */
+
 }})
+let respuesta = []
+for (let i = 0 ; i < finduserid.length; i++) {
+  
+  let usuario = await Users.findByPk(finduserid[i].userId) 
+ 
+  let objeto = {
+    email : usuario.email,
+    picture: usuario.picture,
+    totalamount: finduserid[i].totalamount,
+    updatedAt: finduserid[i].updatedAt
+  } 
+  respuesta.push(objeto)
 
+}
 
-    return res.status(200).json(finduserid);
+    return res.status(200).json(respuesta);
   } catch (error) {
     return res.status(400).json(error)
   }
