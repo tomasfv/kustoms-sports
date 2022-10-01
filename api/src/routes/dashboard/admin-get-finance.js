795da7a87267762dc5376cf9f5249aec2f740121
Kenneth2804 <98670850+Kenneth2804@users.noreply.github.com>
2022-10-01 07:39:24 -0700

    Finance

---
+++ api/src/routes/dashboard/admin-get-finance.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+const {Carts, Products} = require("../../db.js");
+require("dotenv").config();
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  try {
+    let financial = await Carts.findAll({
+      where :{ open: "false"},
+      include :{
+        model: Products
+      },
+    });
+    
+  const finduserid = financial.map(e => { return{
+
+  userId: e.userId,
+  totalamount: e.totalamount,
+  createdAt: e.createdAt,
+  products : e.products.map(t => { return { promotion: t.promotion }})
+
+}})
+
+
+    return res.status(200).json(finduserid);
+  } catch (error) {
+    return res.status(400).json(error)
+  }
+});
+
+
+module.exports = router;
