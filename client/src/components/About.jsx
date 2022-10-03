1d3cd69ce73496031ce6f29a056505b481d99954
Abrahanm7 <abrahanicolasmol98@gmail.com>
2022-10-03 12:22:51 -0300

    Update About.jsx

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
           <h2 class="my-5 text-3xl text-main-dark dark:text-main-light ">Kustoms Sports</h2>
           <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400"></p>
       </div> 
-      <div class="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div class="grid gap-7 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-main-dark dark:text-main-light">
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src={Agus} alt="Agustin Tomas Velazquez"/>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -33,12 +33,12 @@ const About = () => {
                   </li> 
                   <li>
                       <a href="https://github.com/Agusv22" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                          <img class="w-6 h-6 rounded-full bg-main-light" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
                       </a> 
                   </li> 
               </ul>
           </div>
-          <div class="text-center text-gray-500 dark:text-gray-400">
+          <div class="text-center text-main-dark dark:text-main-light">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src={Abrahan} alt="Abraham Nicolas Molina"/>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   <a href="https://www.linkedin.com/in/abraham-molina-8b9711221/">Abraham Nicolas Molina</a>
@@ -52,7 +52,7 @@ const About = () => {
                   </li> 
                   <li>
                       <a href="https://github.com/Abrahanm7" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                          <img class="w-6 h-6 rounded-full bg-main-light " src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
                       </a> 
                   </li> 
               </ul>
@@ -71,26 +71,7 @@ const About = () => {
                   </li> 
                   <li>
                       <a href="https://github.com/tomasfv" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
-                      </a> 
-                  </li> 
-              </ul>
-          </div>
-          <div class="text-center text-gray-500 dark:text-gray-400">
-              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src={Carlos} alt="Carlos Cernigoi"/>
-              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="https://www.linkedin.com/in/carlos-cernigoi-a836164/">Carlos Cernigoi</a>
-              </h3>
-              
-              <ul class="flex justify-center mt-4 space-x-4">
-                  <li>
-                      <a href="https://www.linkedin.com/in/carlos-cernigoi-a836164/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
-                      </a> 
-                  </li> 
-                  <li>
-                      <a href="https://github.com/CarlosCernigoi" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                          <img class="w-6 h-6 rounded-full bg-main-light" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
                       </a> 
                   </li> 
               </ul>
@@ -109,7 +90,26 @@ const About = () => {
                   </li> 
                   <li>
                       <a href="https://github.com/Kenneth2804" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                          <img class="w-6 h-6 rounded-full bg-main-light" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                      </a> 
+                  </li> 
+              </ul>
+          </div>
+          <div class="text-center text-gray-500 dark:text-gray-400">
+              <img class="mx-auto mb-4 w-36 h-36 rounded-full" src={Carlos} alt="Carlos Cernigoi"/>
+              <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <a href="https://www.linkedin.com/in/carlos-cernigoi-a836164/">Carlos José Cernigoi</a>
+              </h3>
+              
+              <ul class="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <a href="https://www.linkedin.com/in/carlos-cernigoi-a836164/" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023323693625127063/linkedin.png'/>
+                      </a> 
+                  </li> 
+                  <li>
+                      <a href="https://github.com/CarlosCernigoi" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
+                          <img class="w-6 h-6 rounded-full bg-main-light" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
                       </a> 
                   </li> 
               </ul>
@@ -128,7 +128,7 @@ const About = () => {
                   </li> 
                   <li>
                       <a href="https://github.com/Max23strm" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                          <img class="w-6 h-6 rounded-full bg-main-light" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
                       </a> 
                   </li> 
               </ul>
@@ -147,7 +147,7 @@ const About = () => {
                   </li> 
                   <li>
                       <a href="https://github.com/lucahaller" class="text-[#ea4c89] hover:text-gray-900 dark:hover:text-white">
-                          <img class="w-6 h-6 rounded-2xl" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
+                          <img class="w-6 h-6 rounded-full bg-main-light" src='https://cdn.discordapp.com/attachments/1016350075192102954/1023325181634166824/github.png'/>
                       </a> 
                   </li>
               </ul>
